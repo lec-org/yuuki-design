aebdde8cda3a5abcf22c761b6e27b16912c77a34
yuuki yuuna <2281363234@qq.com>
2023-09-28 18:27:47 +0800

    fix: 文档配置修复eslint

--- packages/document/.eslintrc.js
+++ packages/document/.eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
-  extends: ['@modern-js']
+  extends: ['@modern-js'],
+  overrides: [
+    {
+      files: ['*.ts', '*.d.ts', '*.tsx'],
+      excludedFiles: ['rspress.config.ts'],
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        project: ['./tsconfig.json']
+      },
+      extends: ['plugin:@typescript-eslint/recommended-requiring-type-checking']
+    }
+  ]
 }
